9c14b36d65ca0f86852eeb63637fefaf0688c260
zarin-raim <zarinar1105@gmail.com>
2017-07-19 18:30:58 +0600

    Plus ball for exam

--- pr3/Program.cs
+++ pr3/Program.cs
@@ -9,6 +9,22 @@ namespace pr3
     {
         static void Main(string[] args)
         {
+            Array array = new Array(3);
+
+            array.Fill();
+            array.Print();
+
+            array.Add(34, 0);
+            array.Add(23, 5);
+            array.Add(65, 5);
+            array.Print();
+
+            array.Remove(65);
+            array.Print();
+
+            Console.WriteLine("On position 4: " + array.Get(4));
+
+            Console.ReadLine();
         }
     }
 }
