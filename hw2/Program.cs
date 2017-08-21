ed4ab1b14329ffd340fb538f8157c325ec569b58
zarin-raim <zarinar1105@gmail.com>
2017-08-21 21:07:44 +0600

    Add homework's task 2

---
+++ hw2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hw2
+{
+    class Program
+    {
+        /*
+            Написать программу, которая обращается к элементам массива по индексу и выходит за его пределы. 
+		    После обработки исключения вывести в финальном блоке сообщение о завершении обработки массива.
+         */
+        static void Main(string[] args)
+        {
+            int[] mas = { 1, 3, 6, 8, 13, 356, 125 };
+
+            try
+            {
+                for(int i = 0; i < 10; i++)
+                {
+                    Console.Write(mas[i] + "\t");
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Выход за границы массива!");
+            }
+            finally
+            {
+                Console.WriteLine("Обработка массива завершена!");
+            }
+
+            Console.ReadLine();
+;        }
+    }
+}
