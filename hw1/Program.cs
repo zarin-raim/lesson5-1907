4aebc2a5998f81708d6e25f19d127356fdf7afd7
zarin-raim <zarinar1105@gmail.com>
2017-08-21 20:48:32 +0600

    Add homework's task 1

---
+++ hw1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace hw1
+{
+    class Program
+    {
+        /*
+            Перехватить исключение запроса к несуществующему веб ресурсу и вывести сообщение о том, что произошла ошибка. 
+     	    Программа должна завершиться без ошибок.
+        */
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input url: ");
+            string url = Console.ReadLine();
+
+            try
+            {
+                Uri uri = new Uri(url);
+                Console.WriteLine("Absoulute path: " + uri.AbsolutePath);
+            }
+            catch (UriFormatException ex)
+            {
+                Console.WriteLine("Invalid url format");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
